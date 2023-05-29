eed82e2dbe9399749f1d2d4f541189bde9ae7519
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-05-29 14:59:04 +0300

    hehe

--- excavator/views.py
+++ excavator/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'excavator/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'excavator/about.html')
+    return render(request, 'about.html')
 
 def excavators(request):
-    return render(request, 'excavator/excavators.html')
+    return render(request, 'excavators.html')
 
 def prices(request):
-    return render(request, 'excavator/prices.html')
+    return render(request, 'prices.html')
 
 def contact(request):
-    return render(request, 'excavator/contact.html')
+    return render(request, 'contact.html')
